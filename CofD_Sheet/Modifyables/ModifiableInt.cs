b1d7e47eadadbf804a2e52e673026faf0d941a08
overheersbeest <gk.alex@live.nl>
2020-07-07 00:17:15 +0200

    fixed save/load crash

--- CofD_Sheet/Modifyables/ModifiableInt.cs
+++ CofD_Sheet/Modifyables/ModifiableInt.cs
@@ -19,6 +19,8 @@ namespace CofD_Sheet.Modifyables
 	[Serializable]
 	public class ModifiableInt
 	{
+		public ModifiableInt()
+		{ }
 		public ModifiableInt(int _defaultValue)
 		{
 			defaultValue = _defaultValue;
